6c17a7143af3135f1eda66d6a1125d0cbb179a0b
Lucas Martinez <lucas.martinez@hpe.com>
2018-11-16 19:33:51 -0600

    patch

--- src/models/StateModel.ts
+++ src/models/StateModel.ts
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
 export interface StateModel<S extends FlatObject = FlatObject> {
-    setState: (key: keyof S, value: ValueType | null) => void;
-    state: Map<keyof S, ValueType | null>;
+    setState: (key: keyof S, value: FlatArray | ValueType | null) => void;
+    state: Map<keyof S, FlatArray | ValueType | null>;
 }
